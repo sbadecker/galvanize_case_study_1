1b18199d2cf346dcd39684cf57da9ce0a53fa9b4
Stefan Decker <stefandecker@Stefans-MacBook-Air.local>
2017-02-10 14:03:01 -0800

    file

--- cleanup.py
+++ cleanup.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import datetime as dt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 
 '''
@@ -12,16 +14,24 @@ year = year[year != 1000]
 price_v_year = df[['SalePrice', 'YearMade']]
 '''
 
-def load_data():
-    df = pd.read_csv('../data/train.zip', compression='zip')
+
+def load_data(max_rows=1000):
+    df = pd.read_csv('../data/train.zip', compression='zip')[:max_rows]
     df['saledate'] = pd.to_datetime(df['saledate'], infer_datetime_format=True)
     df['saleyear'] = df['saledate'].dt.year
     df['ModelID'] = df['ModelID'].astype(str)
     df = df[df['YearMade'] != 1000]
     df['age'] = df['saleyear'] - df['YearMade']
     df = df[df['age'] >= 0]
-    data = df[['ProductGroup','age', 'ProductSize','fiBaseModel','YearMade','ModelID','saleyear','UsageBand','state','SalePrice']]
+    data = df[['ProductGroup','age', 'ProductSize','fiBaseModel','YearMade','ModelID','saleyear','UsageBand','SalePrice']]
+    pd.get_dummies(df, drop_first=True, columns=['ProductGroup','ProductSize','fiBaseModel','ModelID','UsageBand'])
     return data
 
 if __name__ == '__main__':
     data = load_data()
+    y = data.pop('SalePrice')[:1000]
+    X = data[:1000]
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    rand_forest = RandomForestClassifier()
+    rand_forest.fit(X_train, y_train)
+    print rand_forest.score(X_test, y_test)
