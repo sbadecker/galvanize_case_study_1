93028efb3637f3eaabcb32f9c87d63ffa858fb4d
Stefan Decker <stefandecker@Stefans-MacBook-Air.local>
2017-02-10 15:12:53 -0800

    Final model

--- cleanup.py
+++ cleanup.py
@@ -17,15 +17,16 @@ price_v_year = df[['SalePrice', 'YearMade']]
 
 
 def load_data(max_rows=10000):
-    df = pd.read_csv('../data/train.zip', compression='zip')[:10000]
+    df = pd.read_csv('../data/train.zip', compression='zip')
     df['saledate'] = pd.to_datetime(df['saledate'], infer_datetime_format=True)
     df['saleyear'] = df['saledate'].dt.year
     df['ModelID'] = df['ModelID'].astype(str)
     df = df[df['YearMade'] != 1000]
     df['age'] = df['saleyear'] - df['YearMade']
     df = df[df['age'] >= 0]
+    df['age2'] = df['age']**2
     # data = df[['ProductGroup','age', 'ProductSize','fiBaseModel','YearMade','ModelID','saleyear','UsageBand','SalePrice']]
-    data = df[['ProductGroup','age', 'ProductSize','SalePrice', 'YearMade','fiBaseModel']]
+    data = df[['ProductGroup','age', 'age2', 'ProductSize','SalePrice', 'YearMade','fiBaseModel']]
     data = pd.get_dummies(data, drop_first=True, columns=['ProductGroup','ProductSize','fiBaseModel'])
     return data
 
@@ -34,10 +35,10 @@ if __name__ == '__main__':
     y = data.pop('SalePrice')
     X = data
     X_train, X_test, y_train, y_test = train_test_split(X, y)
-    rand_forest = RandomForestRegressor(n_estimators=500, oob_score=True)
+    rand_forest = RandomForestRegressor(n_estimators=40, oob_score=True, n_jobs=4)
     rand_forest.fit(X_train, y_train)
     print rand_forest.score(X_test, y_test)
-    # for model in [LassoCV, RidgeCV, LinearRegression]:
-    #     mod = model()
-    #     mod.fit(X_train, y_train)
-    #     print str(model), mod.score(X_test, y_test)
+    for model in [LassoCV, RidgeCV, LinearRegression]:
+         mod = model()
+         mod.fit(X_train, y_train)
+         print str(model), mod.score(X_test, y_test)
