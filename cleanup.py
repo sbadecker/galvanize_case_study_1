18ccf467df5ade2bf130c5d34610332dc66f8686
Stefan Decker <stefandecker@Stefans-MacBook-Air.local>
2017-02-10 15:35:29 -0800

    Final final

--- cleanup.py
+++ cleanup.py
@@ -16,29 +16,49 @@ price_v_year = df[['SalePrice', 'YearMade']]
 '''
 
 
-def load_data(max_rows=10000):
-    df = pd.read_csv('../data/train.zip', compression='zip')
+def load_data(filename, max_rows=10000):
+    df = pd.read_csv(filename, compression='zip')
     df['saledate'] = pd.to_datetime(df['saledate'], infer_datetime_format=True)
     df['saleyear'] = df['saledate'].dt.year
     df['ModelID'] = df['ModelID'].astype(str)
     df = df[df['YearMade'] != 1000]
     df['age'] = df['saleyear'] - df['YearMade']
     df = df[df['age'] >= 0]
-    df['age2'] = df['age']**2
     # data = df[['ProductGroup','age', 'ProductSize','fiBaseModel','YearMade','ModelID','saleyear','UsageBand','SalePrice']]
-    data = df[['ProductGroup','age', 'age2', 'ProductSize','SalePrice', 'YearMade','fiBaseModel']]
+    data = df[['ProductGroup','age', 'ProductSize','SalePrice', 'YearMade','fiBaseModel']]
+    data = pd.get_dummies(data, drop_first=True, columns=['ProductGroup','ProductSize','fiBaseModel'])
+    return data
+
+def load_test_data(filename, max_rows=10000):
+    df = pd.read_csv(filename, compression='zip')
+    df['saledate'] = pd.to_datetime(df['saledate'], infer_datetime_format=True)
+    df['saleyear'] = df['saledate'].dt.year
+    df['ModelID'] = df['ModelID'].astype(str)
+    df['age'] = df['saleyear'] - df['YearMade']
+    # data = df[['ProductGroup','age', 'ProductSize','fiBaseModel','YearMade','ModelID','saleyear','UsageBand','SalePrice']]
+    data = df[['ProductGroup','age', 'ProductSize', 'YearMade','fiBaseModel']]
     data = pd.get_dummies(data, drop_first=True, columns=['ProductGroup','ProductSize','fiBaseModel'])
     return data
 
 if __name__ == '__main__':
-    data = load_data()
+    data = load_data('../data/train.zip')[:1000]
+    test_data = load_test_data('../data/test.zip')[:1000]
     y = data.pop('SalePrice')
     X = data
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
-    rand_forest = RandomForestRegressor(n_estimators=40, oob_score=True, n_jobs=4)
-    rand_forest.fit(X_train, y_train)
-    print rand_forest.score(X_test, y_test)
-    for model in [LassoCV, RidgeCV, LinearRegression]:
-         mod = model()
-         mod.fit(X_train, y_train)
-         print str(model), mod.score(X_test, y_test)
+    X_final = test_data
+    rand_forest = RandomForestRegressor(n_estimators=250, oob_score=True, n_jobs=4)
+    rand_forest.fit(X, y)
+    y_pred = rand_forest.predict(X_final)
+    final_df = pd.DataFrame([test_data['SalesID'],y_pred])
+    final_df.to_csv('output.csv')
+    # rand_forest.fit(X_train, y_train)
+
+    # X_train, X_test, y_train, y_test = train_test_split(X, y)
+    # rand_forest = RandomForestRegressor(n_estimators=400, oob_score=True, n_jobs=4)
+    # rand_forest.fit(X_train, y_train)
+    #
+    # print rand_forest.score(X_test, y_test)
+    # for model in [LassoCV, RidgeCV, LinearRegression]:
+    #      mod = model()
+    #      mod.fit(X_train, y_train)
+    #      print str(model), mod.score(X_test, y_test)
